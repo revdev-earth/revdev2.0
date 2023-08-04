0f2c05a86fa47fb561b2219bed4df93a4330569a
revione <revione@outlook.com>
2023-08-04 15:11:25 +0200

    vamo

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,21 @@ import Cards2 from "@/components/Cards2";
 
 const staggerDelay = 0.3; // Retraso entre la animación de cada div en segundos
 
-const divVariants = {
+const divVariant = (delay: number) => ({
   hidden: {
     opacity: 0,
     x: "25%", // Empieza fuera de la pantalla a la derecha
   },
   visible: {
-    opacity: 1,
-    x: "0%", // Llega a su posición original a la izquierda
+    opacity: [0, 1, 1, 0, 0],
+    x: ["25%", "0%", "0%", "-50%", "25%"], // Llega a su posición original a la izquierda
+    transition: {
+      duration: 10,
+      delay,
+      repeat: Infinity,
+    },
   },
-};
+});
 
 export default function Home() {
   return (
@@ -28,36 +33,28 @@ export default function Home() {
         <motion.div
           initial="hidden"
           animate="visible"
-          variants={divVariants}
-          transition={{
-            duration: 1, // Duración de la animación de cada div en segundos
-          }}
+          variants={divVariant(0)}
           style={{ marginBottom: "1rem" }} // Espacio entre cada div
         >
-          Logra lo que deseas
+          {/* Logra lo que deseas <br /> */}
+          Erreiche, was du wünschst
         </motion.div>
         <motion.div
           initial="hidden"
           animate="visible"
-          variants={divVariants}
-          transition={{
-            duration: 1,
-            delay: staggerDelay, // Retraso entre la animación de este div y el anterior
-          }}
+          variants={divVariant(staggerDelay)}
           style={{ marginBottom: "1rem" }}
         >
-          Esta a unos pasos
+          {/* Esta a unos pasos <br /> */}
+          Es ist nur noch ein paar Schritte entfernt
         </motion.div>
         <motion.div
           initial="hidden"
           animate="visible"
-          variants={divVariants}
-          transition={{
-            duration: 1,
-            delay: staggerDelay * 2, // Retraso entre la animación de este div y el anterior
-          }}
+          variants={divVariant(staggerDelay * 2)}
         >
-          Hagamos que suceda
+          {/* Hagamos que suceda <br /> */}
+          Lass uns das geschehen
         </motion.div>
       </div>
 
