f949cee7e5202f301b49a569d76356a527550777
Nickothan <s1nicolasrg@gmail.com>
2023-08-13 11:08:59 -0500

    estilizando header.

--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -5,7 +5,7 @@ export default function Services () {
     return (
         <div className='flex flex-col justify-center items-center'>
         <h3 className='text-center text-3xl font-bold'>services</h3>
-        <ul className='w-full grid gap-7 place-content-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        <ul className='w-full lg:w-[1000px] grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             {
                 data.services.map((service) => {
                     return (
